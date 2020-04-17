410179ca0bbf01344fbafae7061ff70681e8aac6
dsouza <sys23@grupopmz.com.br>
2020-04-17 16:02:16 -0400

    DatabaseSeeder
    Adicionado as Seeders:
    * PerfilSeeder;
    * TipoPessoaSeeder;
    * PessoaSeeder;
    * UserSeeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call([
+             PerfilSeeder::class,
+             TipoPessoaSeeder::class,
+             PessoaSeeder::class,
+             UserSeeder::class,
+         ]);
     }
 }
