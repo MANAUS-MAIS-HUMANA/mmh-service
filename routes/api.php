35cb8875631d78c66e2ea1e76400a3415d1de8e7
dsouza <sys23@grupopmz.com.br>
2020-04-19 21:39:44 -0400

    Removido comentário desnecessário

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->middleware('api')->group(function () {
     /** @api /api/v1/auth */
     Route::prefix('auth')->group(function () {
-        /** @api /api/v1/auth/create */
         Route::post('create', 'AuthController@create')->name('user.create');
         Route::post('password-reset', 'AuthController@passwordReset')->name('user.passwordReset');
     });
