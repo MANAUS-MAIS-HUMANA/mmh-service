cbd990b61d78e113a40669ce45e3ab3ef30a7a0b
dsouza <sys23@grupopmz.com.br>
2020-04-22 11:17:48 -0400

    Adicionado RedefinirSenha
    Adicionado classe responsável por enviar o e-mail de redefinição de senha ao usuário.

--- app/Mail/RedefinirSenha.php
+++ app/Mail/RedefinirSenha.php
@@ -2,23 +2,36 @@
 
 namespace App\Mail;
 
+use App\Models\User;
+use App\Models\RedefinirSenha As RedefinirSenhaModel;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class RedefinirSenha extends Mailable
+class RedefinirSenha extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
     /**
+     * @var User
+     */
+    private User $user;
+    /**
+     * @var RedefinirSenhaModel
+     */
+    private RedefinirSenhaModel $redefinirSenha;
+
+    /**
      * Create a new message instance.
      *
-     * @return void
+     * @param User $user
+     * @param RedefinirSenhaModel $redefinirSenha
      */
-    public function __construct()
+    public function __construct(User $user, RedefinirSenhaModel $redefinirSenha)
     {
-        //
+        $this->user = $user;
+        $this->redefinirSenha = $redefinirSenha;
     }
 
     /**
@@ -28,6 +41,12 @@ class RedefinirSenha extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->subject('Redefinição de Senha')
+            ->markdown('emails.redefinir_senha')
+            ->with([
+                'user' => $this->user,
+                'redefinirSenha' => $this->redefinirSenha,
+                'token' => base64_encode("{$this->redefinirSenha->email}&&{$this->redefinirSenha->token}&&{$this->redefinirSenha->validade}")
+            ]);
     }
 }
