a8c9f9d2f8c80c11f71206cdf836cd40ce91a159
Fernando Barreto da Costa <f.costa@accesscredit.com.br>
2020-04-23 09:03:16 -0400

    Inserindo a variavel FRONT_URL no .env e configurando o cors para buscar a mesma

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://front.localhost:8000'],
+    'allowed_origins' => [env('FRONT_URL')],
+    // 'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => ['*'],
 
