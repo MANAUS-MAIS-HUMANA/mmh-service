d31bc6a49daeb0e5f2d721ba9a4cb1563e8e9791
dsouza <sys23@grupopmz.com.br>
2020-04-23 16:39:31 -0400

    Usuário

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 /** @api /api/v1/ */
 Route::prefix('v1')->middleware('api')->group(function () {
-    /** @api /api/v1/auth */
-    Route::prefix('auth')->group(function () {
-        Route::post('create', 'AuthController@create')->name('auth.create');
-        Route::post('password-reset', 'AuthController@passwordReset')->name('auth.passwordReset');
-        Route::post('confirm-password-reset', 'AuthController@confirmPasswordReset')->name('auth.confirmPasswordReset');
+
+    /** @api /api/v1/usuario */
+    Route::prefix('usuario')->group(function () {
+        Route::post('create', 'UsuarioController@create')->name('usuario.create');
+        Route::post('password-reset', 'UsuarioController@passwordReset')->name('usuario.passwordReset');
+        Route::post('confirm-password-reset', 'UsuarioController@confirmPasswordReset')->name('usuario.confirmPasswordReset');
     });
+
 });
