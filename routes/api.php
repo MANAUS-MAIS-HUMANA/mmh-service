2d31cde5de8ef92e8e5e88435f8aae3de5efa475
dsouza <sys23@grupopmz.com.br>
2020-05-02 18:38:09 -0400

    Movido métodos de usuário para o AuthController

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    /** @api /api/v1/auth */
+    Route::prefix('auth')->group(function () {
+        Route::post('login', 'AuthController@login')->name('auth.login');
+        Route::post('logout', 'AuthController@logout')->name('auth.logout');
+        Route::post('create', 'UsuarioController@create')->name('auth.create');
+        Route::post('password-reset', 'UsuarioController@passwordReset')->name('auth.passwordReset');
+        Route::post('confirm-password-reset', 'UsuarioController@confirmPasswordReset')->name('auth.confirmPasswordReset');
+    });
+
+    /** @api /api/v1/usuario */
+    Route::prefix('usuario')->group(function () {
+        Route::post('/', 'UsuarioController@getAll')->name('usuario.getAll');
+    });
+
 });
