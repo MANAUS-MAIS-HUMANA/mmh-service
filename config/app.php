67544b799e6453076ccaa612fe95a3a34ff1bf2c
Bruno Campos <brunocampos@meliuz.com.br>
2020-08-30 22:45:26 -0400

    Fazer backup do banco de dados
    Configurar o Laravel para fazer backup do banco de dados diariamente
    para o Google Drive. Além disso, enviar uma notificação pelo Slack, no
    canal "backups" toda vez que um backup for gerado.

--- config/app.php
+++ config/app.php
@@ -175,7 +175,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\GoogleDriveServiceProvider::class,
 
+        Spatie\Backup\BackupServiceProvider::class,
     ],
 
     /*
